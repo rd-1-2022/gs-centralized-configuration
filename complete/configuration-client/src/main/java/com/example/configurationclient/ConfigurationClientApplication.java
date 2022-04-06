75cbd644770e48770e1de8b701d88e95944d8a51
Mark Pollack <mpollack@vmware.com>
2022-04-06 12:57:36 -0400

    add test annotation property

--- complete/configuration-client/src/main/java/com/example/configurationclient/ConfigurationClientApplication.java
+++ complete/configuration-client/src/main/java/com/example/configurationclient/ConfigurationClientApplication.java
@@ -20,7 +20,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class ConfigurationClientApplication {
 
 	public static void main(String[] args) {
